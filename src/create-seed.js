a9c6d135f62d327f5ea93208363543a5f26b6fb7
mrtn28 <103893626+mrtn28@users.noreply.github.com>
2023-02-28 22:51:18 +0000

    create-seed

---
+++ src/create-seed.js
@@ -0,0 +1,21 @@
+import { cancel, intro, isCancel, outro, select } from '@clack/prompts'
+import bip39 from 'bip39'
+
+intro('Welcome.')
+
+const strength = await select({
+  message: 'Pick a number of words.',
+  options: [
+    { value: '128', label: '12' },
+    { value: '256', label: '24' }
+  ]
+})
+
+if (isCancel(strength)) {
+  cancel('Operation cancelled.')
+  process.exit(0)
+}
+
+const mnemonic = bip39.generateMnemonic(strength)
+
+outro(mnemonic)
